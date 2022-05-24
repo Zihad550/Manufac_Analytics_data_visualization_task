26f7f4c08d79cf9769ed678a182894987c045ff3
Zihad550 <hussainmuzahid83@gmail.com>
2022-05-24 12:40:50 +0600

    fix: charts added

---
+++ src/components/Chart/Chart.tsx
@@ -0,0 +1,57 @@
+import ReactECharts from "echarts-for-react";
+import React from "react";
+
+interface ChartProps {
+  xValues: {
+    data: number[];
+    name: string;
+  };
+  yValues: {
+    data: number[];
+    name: string;
+  };
+  title: string;
+  type: string;
+}
+
+const Chart = ({ xValues, yValues, title, type }: ChartProps) => {
+  console.log(title);
+  const option = {
+    title: {
+      text: title,
+    },
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "shadow",
+      },
+    },
+    legend: {
+      data: [title],
+    },
+    xAxis: {
+      name: xValues.name,
+      data: xValues.data,
+    },
+    yAxis: {
+      name: yValues.name,
+    },
+    series: [
+      {
+        name: title,
+        type: type,
+        data: yValues.data,
+      },
+    ],
+  };
+
+  return (
+    <ReactECharts
+      option={option}
+      style={{ height: 600 }}
+      opts={{ renderer: "svg" }}
+    />
+  );
+};
+
+export default Chart;
